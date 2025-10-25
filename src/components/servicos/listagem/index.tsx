c760343018caf911dd644786b8ee03dcb2a0276d
Pietro-dev <pietro.pietromartins@gmail.com>
2025-10-25 11:10:02 -0300

    feat: criação da tabela de listagem de serviços

--- src/components/servicos/listagem/index.tsx
+++ src/components/servicos/listagem/index.tsx
@@ -1,12 +1,28 @@
 import { Layout } from 'components/layout'
 import Link from 'next/link'
+import { TabelaServicos } from './tabela'
+import { Servico } from 'app/models/servicos'
 
 export const ListagemServicos: React.FC = () => {
+
+    const servicos: Servico[] = [{
+        id: '1', servico: 'teste', descricao: 'teste', preco: 50, duracao: 30
+    },{
+        id: '2', servico: 'teste', descricao: 'teste', preco: 50, duracao: 30
+    },{
+        id: '3', servico: 'teste', descricao: 'teste', preco: 50, duracao: 30
+    },{
+        id: '4', servico: 'teste', descricao: 'teste', preco: 50, duracao: 30
+    }
+    ]
+    
     return(
-        <Link href="/cadastros/servicos">
-            <Layout titulo='Serviços cadastrados'>
+        <Layout titulo='Serviços cadastrados'>
+            <Link href="/cadastros/servicos">
+                <TabelaServicos servicos={servicos}/>
                 <button className="button is-primary is-dark">Novo</button>
-            </Layout>
-        </Link>
+            </Link>
+        </Layout>
+ 
     )
 }
---
+++ src/components/servicos/listagem/tabela/index.tsx
@@ -0,0 +1,51 @@
+import { Servico } from 'app/models/servicos'
+
+interface TabelaServicosProps {
+    servicos: Array<Servico>
+}
+
+export const TabelaServicos: React.FC<TabelaServicosProps> = ({
+    servicos
+}) => {
+    return(
+        <table className="table is-striped is-hoverable is-fullwidth">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Servico</th>
+                    <th>Descrição</th>
+                    <th>Preço</th>
+                    <th>Duração</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {servicos.map(servico => <ServicoRow key={servico.id} servico={servico}/>)}
+            </tbody>
+        </table>
+    )
+}
+
+interface ServicoRowProps {
+    servico: Servico
+}
+
+const ServicoRow: React.FC<ServicoRowProps> = ({
+    servico
+}) => {
+    return (
+        <tr>
+            <td>{servico.id}</td>
+            <td>{servico.servico}</td>
+            <td>{servico.descricao}</td>
+            <td>{servico.preco}</td>
+            <td>{servico.duracao}</td>
+            <td>
+                <div className='buttons'>
+                    <button className='button is-warning is-dark'>Editar</button>
+                    <button className='button is-danger is-dark'>Deletar</button>
+                </div>
+            </td>
+        </tr>
+    )
+}
